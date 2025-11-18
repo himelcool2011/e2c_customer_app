ff72433bf1e77fcb8e0c47054c0a584834a2b50f
mehearaz <mu.himel@gmail.com>
2025-11-18 16:04:31 +0600

    initiate

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns:[
+      {
+        protocol : 'https',
+        hostname: 'www.portotheme.com',
+        pathname: '/wordpress/porto/**'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
